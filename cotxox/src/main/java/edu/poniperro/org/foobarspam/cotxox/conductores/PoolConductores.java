06a4b976f00cdaa05d62d8e60e21829493a4b7d4
ErikPC <epardillo@cifpfbmoll.eu>
2022-02-08 21:34:52 +0100

    feat: crear PoolConductores sin acabar

---
+++ cotxox/src/main/java/edu/poniperro/org/foobarspam/cotxox/conductores/PoolConductores.java
@@ -0,0 +1,17 @@
+package edu.poniperro.org.foobarspam.cotxox.conductores;
+
+public class PoolConductores {
+    private List<Conductor> = new ArrayList(PoolConductores);
+
+    PoolConductores(List<Conductor> poolConductores){
+        this.poolConductores = poolConductores;
+    }
+
+    public List<conductores> getPoolConductores() {
+        return this.poolConductores;
+    }
+
+    public asignarConductor(){
+
+    }
+}
