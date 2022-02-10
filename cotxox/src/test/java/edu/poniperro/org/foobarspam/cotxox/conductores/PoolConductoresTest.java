a935ccfd0692919dacefa82498c2de4abcf2870c
ErikPC <epardillo@cifpfbmoll.eu>
2022-02-10 01:59:14 +0100

    tests: crear test clase poolConductores

---
+++ cotxox/src/test/java/edu/poniperro/org/foobarspam/cotxox/conductores/PoolConductoresTest.java
@@ -0,0 +1,32 @@
+package edu.poniperro.org.foobarspam.cotxox.conductores;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+import java.util.Optional;
+
+import org.junit.Test;
+
+public class PoolConductoresTest {
+    @Test
+    public void asignarCopnductorTest() {
+        ArrayList<Conductor> samuele = new ArrayList<Conductor>() {
+            {
+                add(new Conductor("Samuele"));
+                add(new Conductor("Jorge"));
+                add(new Conductor("David"));
+                add(new Conductor("Falopio"));
+            }
+        };
+        PoolConductores mejoresConductores = new PoolConductores(samuele);
+        Optional<Conductor> condutorElejido = mejoresConductores.asignarConductor();
+        assertTrue(condutorElejido.isPresent());
+        assertTrue(
+                condutorElejido.get().getNombre() == "Samuele"
+                        || condutorElejido.get().getNombre() == "Falopio"
+                        || condutorElejido.get().getNombre() == "Jorge"
+                        || condutorElejido.get().getNombre() == "David");
+
+    }
+}
