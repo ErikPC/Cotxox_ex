7f55091897ca51da1fd910fd7e530b71b14422a7
ErikPC <epardillo@cifpfbmoll.eu>
2022-02-11 00:36:58 +0100

    fix: cambiar visibilidad metodos

--- cotxox/src/main/java/edu/poniperro/org/foobarspam/cotxox/tarifa/Tarifa.java
+++ cotxox/src/main/java/edu/poniperro/org/foobarspam/cotxox/tarifa/Tarifa.java
@@ -1,20 +1,22 @@
 package edu.poniperro.org.foobarspam.cotxox.tarifa;
 
+import edu.poniperro.org.foobarspam.cotxox.carrera.Carrera;
+
 public class Tarifa {
     static final double COSTE_MILLA = 1.35;
     static final double COSTE_MINUTO = 0.35;
     static final double COSTE_MINIMO = 5.0;
     static final byte PORCENTAJE_COMISION = 20;
 
-    static double getCosteDistancia(double distancia) {
+    public static double getCosteDistancia(double distancia) {
         return distancia * COSTE_MILLA;
     }
 
-    static double getCosteTiempo(int tiempo) {
+    public static double getCosteTiempo(int tiempo) {
         return tiempo * COSTE_MINUTO;
     }
 
-    static double getCosteTotalEsperado(Carrera carrera) {
+    public static double getCosteTotalEsperado(Carrera carrera) {
         double esperado = getCosteDistancia(carrera.getDistancia()) + getCosteTiempo(carrera.getTiempoEsperado());
         return esperado <= COSTE_MINIMO ? COSTE_MINIMO : esperado;
     }
